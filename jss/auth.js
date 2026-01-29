81e23cc41551c7f9ac25ec701991ff2c852418d7
myw4371-a11y <myw4371@gmail.com>
2026-01-30 04:06:23 +0600

    Implement user registration and logout functions

--- jss/auth.js
+++ jss/auth.js
@@ -1,1 +1,35 @@
+// js/auth.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { firebaseConfig } from './config.js';
 
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+// রেজিস্ট্রেশন ফাংশন
+export const registerUser = async (user, pass) => {
+    const fakeEmail = user + "@ffcbd.com";
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, fakeEmail, pass);
+        const uid = userCredential.user.uid;
+        await set(ref(db, 'users/' + uid), {
+            username: user,
+            balance: 0,
+            password: pass,
+            profilePic: "default"
+        });
+        return { success: true };
+    } catch (error) {
+        return { success: false, message: error.message };
+    }
+};
+
+// লগআউট ফাংশন
+export const logoutUser = () => {
+    signOut(auth).then(() => {
+        localStorage.clear();
+        window.location.href = '../index.html';
+    });
+};
