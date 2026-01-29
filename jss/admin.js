38f4548d39daff9650aef8bd2a4f740be45739a7
myw4371-a11y <myw4371@gmail.com>
2026-01-30 04:06:47 +0600

    Add functions for match and user management

--- jss/admin.js
+++ jss/admin.js
@@ -1,1 +1,22 @@
+// js/admin.js
+import { getDatabase, ref, push, update, remove, get } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
+// নতুন ম্যাচ অ্যাড করা
+export const createMatch = (db, type, matchData) => {
+    return push(ref(db, 'tournaments/' + type), matchData);
+};
+
+// ইউজার ব্যালেন্স আপডেট করা
+export const setUserBalance = (db, uid, amount) => {
+    return update(ref(db, 'users/' + uid), { balance: amount });
+};
+
+// ম্যাচ ডিলিট করা
+export const removeMatch = (db, type, id) => {
+    return remove(ref(db, `tournaments/${type}/${id}`));
+};
+
+// সেটিংস আপডেট করা
+export const updateSettings = (db, data) => {
+    return update(ref(db, 'adminInfo'), data);
+};
